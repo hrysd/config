eea0a1f017aaec1113bc76798d456e35eabb0fc6
Hiroshi Yoshida <hrysd22@gmail.com>
2015-08-11 18:51:35 +0900

    Hi, rcm!

--- dotfiles/setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for file in .gemrc .gitconfig .gitignore .pryrc .tmux.conf .vimrc .zshrc .zprofile .zsh
-do
-  rm -r  $HOME/$file
-  echo 'making symlink' $file '->' $HOME/$file
-  ln -s `pwd`/$file $HOME/$file
-done
